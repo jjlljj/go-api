6c436496d3466c524b20afcd7030e39299b496ea
James Logue <logueja@gmail.com>
2018-05-18 10:52:57 -0600

    Add initial model, app structure

--- app.go
+++ app.go
@@ -1,2 +1,19 @@
-tyoe App struct {
+package main
+
+import (
+  "database/sql"
+
+  "github.com/gorilla/mux"
+   _ "github.com/lib/pq"
+)
+
+type App struct {
+  Router *mux.Router
+  DB *sql.DB
 }
+
+// initialize => create db connection && routes
+func (a *App) Initialize(user, password, dbname string) { }
+
+// start application
+func (a *App) Run(addr string) { }
