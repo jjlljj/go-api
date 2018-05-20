bd101dd1712a64ffdc95183958d83ed4c044f639
James Logue <logueja@gmail.com>
2018-05-20 10:07:27 -0600

    Begin initialize db

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "database/sql"
+  "fmt"
+  "log"
 
   "github.com/gorilla/mux"
    _ "github.com/lib/pq"
@@ -13,7 +15,18 @@ type App struct {
 }
 
 // initialize => create db connection && routes
-func (a *App) Initialize(user, password, dbname string) { }
+func (a *App) Initialize(user, password, dbname string) { 
+  connectionString := fmt.Sprintf("user=%s password=%s dbname=%s", user, password, dbname)
+
+  var err error
+
+  a.DB, err = sql.Open("postgres", connectionString)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  a.Router = mux.NewRouter()
+}
 
 // start application
 func (a *App) Run(addr string) { }
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "os"
 
 func main() {
   a := App{}
-  a.initialize(
+  a.Initialize(
     os.Getenv("APP_DB_USERNAME"),
     os.Getenv("APP_DB_PASSWORD"),
     os.Getenv("APP_DB_NAME"))
